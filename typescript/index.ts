e0ce6eefdec83db7fd1e8585b8fe036701972f97
Masayuki Kawamura <kwmr5864@gmail.com>
2015-12-12 13:12:18 +0900

    位置情報クラスを分離

--- typescript/index.ts
+++ typescript/index.ts
@@ -16,25 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-class GeoPosition {
-	private geolocation: Geolocation = null
-	private options: {string?: string} = {}
-	constructor() {
-		if (navigator.geolocation) {
-			this.geolocation = navigator.geolocation
-		}
-	}
-	getPosition() {
-		if (!this.geolocation) { return }
-		this.geolocation.getCurrentPosition(this.successCallback, this.errorCallback, this.options)
-	}
-	private successCallback(position: Position) {
-		alert(`${position.coords.latitude}, ${position.coords.longitude}`)
-	}
-	private errorCallback(error) {
-		alert('お使いのアプリでは位置情報を取得できません')
-	}
-}
+/// <reference path="GeoPosition.ts" />
 
 var geoPosition = new GeoPosition()
 
