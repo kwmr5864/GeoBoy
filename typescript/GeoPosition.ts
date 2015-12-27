fbf3eaf01d82f468ebc22e8ae3936f04c9c4d875
Masayuki Kawamura <kwmr5864@gmail.com>
2015-12-27 20:48:58 +0900

    位置情報の代わりに、過去に取得した位置をマップ表示

--- typescript/GeoPosition.ts
+++ typescript/GeoPosition.ts
@@ -1,6 +1,21 @@
 class GeoPosition {
     private geolocation: Geolocation = null
     private watchId: number = null
+
+    static showPosition(lat: number, lon: number) {
+        var position = new google.maps.LatLng(lat, lon)
+        var map = new google.maps.Map($('#map')[0], {
+            zoom: 14,
+            center: position,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            scaleControl: true
+        })
+        var marker = new google.maps.Marker({
+            map: map,
+            position: position
+        })
+    }
+
     constructor() {
         if (navigator.geolocation) {
             this.geolocation = navigator.geolocation
@@ -29,17 +44,7 @@ class GeoPosition {
         let lon = position.coords.longitude
         let index = appStorage.getIndex()
         appStorage.addLog(new Log(index, lat, lon))
-        var position = new google.maps.LatLng(lat, lon)
-        var map = new google.maps.Map($('#map')[0], {
-            zoom: 14,
-            center: position,
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            scaleControl: true
-        })
-        var marker = new google.maps.Marker({
-            map: map,
-            position: position
-        })
+        GeoPosition.showPosition(lat, lon)
     }
     private errorCallback(error) {
         alert('お使いのアプリでは位置情報を取得できません')
